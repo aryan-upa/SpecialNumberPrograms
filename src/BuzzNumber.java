96d13d72bcf46d2b0764af4ad74f8f54df8390d7
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-05 21:47:03 +0530

    Added implementation of Buzz Number Program in Java language ❤.

---
+++ src/BuzzNumber.java
@@ -0,0 +1,9 @@
+public class BuzzNumber {
+
+    public static boolean isBuzzNumber(long num) {
+        if(num < 7)
+            return false;
+        else return num % 7 == 0 || num % 10 == 7;
+    }
+
+}
