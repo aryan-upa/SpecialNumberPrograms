28c0f418bc6357d177d0a1b3e50642da036a3b04
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-05 18:41:25 +0530

    Added implementation of Prime Number Program in Java language ❤.

---
+++ src/PrimeNumber.java
@@ -0,0 +1,18 @@
+public class PrimeNumber {
+
+    public static boolean isPrime(long num) {
+        if(num <= 1)
+            return false;
+        else if (num == 2 || num == 3)
+            return true;
+        else if (num % 2 == 0 || num % 3 == 0)
+            return false;
+        else {
+            for (int i = 5; (long) i * i < num; i += 6)
+                if (num % i == 0 || num % (i+2) == 0)
+                    return false;
+        }
+        return true;
+    }
+
+}
