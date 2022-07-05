dd0279c09d0190a2045f25101597f901de0517d2
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-05 23:14:40 +0530

    Added implementation of Harshad Number Program in Java language ❤.

---
+++ src/HarshadNumber.java
@@ -0,0 +1,15 @@
+public class HarshadNumber {
+
+    public static boolean isHarshadNumber(long num) {
+        int sumOfDigits = 0;
+        long tempNum = num;
+
+        while(tempNum > 0) {
+            sumOfDigits += tempNum % 10;
+            tempNum /= 10;
+        }
+
+        return num % sumOfDigits == 0;
+    }
+
+}
