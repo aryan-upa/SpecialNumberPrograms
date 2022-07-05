dbcd9ffd7a86af1a2ea9855c822e7410a2d70021
Aryan Upadhyay <aryan.offc@gmail.com>
2022-07-05 23:09:07 +0530

    Added implementation of Armstrong Number Program in Java language ❤.

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,17 @@
+public class ArmstrongNumber {
+
+    public static boolean isArmstrongNumber(long num) {
+        if(num < 0)
+            return false;
+
+        String numStr = String.valueOf(num);
+        int noOfDigits = numStr.length();
+        long sum = 0;
+
+        for(char c : numStr.toCharArray())
+            sum = sum + (long) Math.pow(c-'0', noOfDigits);
+
+        return sum == num;
+    }
+
+}
