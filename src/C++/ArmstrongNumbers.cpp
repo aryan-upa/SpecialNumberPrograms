fb2d9cdbdfbd18156c1afe9a8d5fe800a7d2e55b
Moksh Upadhyay <mokshatwork@gmail.com>
2022-07-08 21:48:42 +0530

    Modified the Inmplementation of Armstrong Numbers in C++

--- src/C++/ArmstrongNumbers.cpp
+++ src/C++/ArmstrongNumbers.cpp
@@ -3,32 +3,7 @@ using namespace std;
 bool isArmstrong(long n);
 
 int main(){
-    int n,flag;
-//     cout << "Enter a number: ";
-//     cin >> n;
-//     if (n < 0)
-//     {
-//         return false;
-//     }
-//     int sum = 0;
-//     int temp = n;
-//     while (temp > 0){
-//         int digit = temp % 10;
-//         sum += digit * digit * digit;
-//         temp /= 10;
-//     }
-//     if (sum == n)
-//     {
-//         cout << "Yes";
-//     }
-//     else
-//     {
-//         cout << "No";
-//     }
-//     return 0;
-    cout << "Enter a number: ";
-    cin >> n;
-    flag = isArmstrong(n);
+    bool flag = isArmstrong(155);
     if(flag == true)
         cout<<"Number is an Armstrong Number.";
     else
@@ -37,7 +12,6 @@ int main(){
 }
 
 bool isArmstrong(long n) {
-    
     bool isArms;
     if (n < 0) {
         return false;
