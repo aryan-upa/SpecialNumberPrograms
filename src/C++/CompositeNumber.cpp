68b3043d4790c48a2331fb80188f52cdcddf2c76
Moksh Upadhyay <mokshatwork@gmail.com>
2022-07-31 13:18:07 +0530

    Made some changes to the Composite Number Program

--- src/C++/CompositeNumber.cpp
+++ src/C++/CompositeNumber.cpp
@@ -3,15 +3,17 @@ using namespace std;
 bool isComposite(long n);
 
 bool isComposite(long n){
+    if(n<1){
+        return false;
+    }
     int count= 0;
-    for(int i = 2; i<=n; i++){
+    for(int i = 1; i*i<=n; i++){
         if(n%i==0){
             count++;
         }
     }
-    
     if(count>2){
-        return true;
+        return true;e
     }
     else{
         return false;
