236fe13999a378feadd587bce9def712c8f0c022
Moksh Upadhyay <mokshatwork@gmail.com>
2022-08-01 13:51:51 +0530

    Added implementation of Disarium Number in C++

--- src/C++/DisariumNumber.cpp
+++ src/C++/DisariumNumber.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+int countDigits(int n);
+bool isDisariumNumber(long n);
 
-int main(){
-    cout<<"Hello World!"<<endl;
+
+int countDigits(int n){
+    int count = 0;
+    while (n != 0) {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+
+bool isDisariumNumber(long n){
+    int count = countDigits(n);
+    int sum = 0;
+    int temp = n;
+    while (temp != 0){
+        int digit = temp % 10;
+        sum += pow(digit, count--);
+        temp = temp / 10;
+    }
+    if (sum == n)
+        return true;
+    else
+        return false;
 }
