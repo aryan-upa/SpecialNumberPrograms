445a4a7a19348d46e40c128a713f26a138750452
Moksh Upadhyay <mokshatwork@gmail.com>
2022-09-28 22:40:04 +0530

    Updated the Implementation

--- src/C++/HarshadNumber.cpp
+++ src/C++/HarshadNumber.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
-<<<<<<< HEAD
-using namespace std;
-bool isHarshadNumber(long n);
-
-=======
 
 using namespace std;
 bool isHarshadNumber(long n);
 
-int main()
-{
-    cout << "Harshad Number" << endl;
-    if (isHarshadNumber(24))
-        cout << "Yes , it is harshad Number" << endl;
-    else
-        cout << "not";
-}
-
->>>>>>> 51813d2082c71ddf2bddae7cbd6cbc4adcfdf1dd
 bool isHarshadNumber(long n)
 {
     int sumOfDigits = 0;
     long tempNum = n;
-<<<<<<< HEAD
-=======
-
->>>>>>> 51813d2082c71ddf2bddae7cbd6cbc4adcfdf1dd
     while (tempNum > 0)
     {
         sumOfDigits += tempNum % 10;
