63adb96087bb3d2365250ebb1e14876d066216ab
Moksh Upadhyay <mokshatwork@gmail.com>
2022-10-09 22:30:28 +0530

    Correct Program name

---
+++ src/C++/SpyNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+bool isSpyNumber(long n);
+
+bool isSpyNumber(long n) {
+    long digit, sum = 0, product = 1;
+    while (n > 0) {
+        digit = (int)n % 10;
+        sum += digit;
+        product *= digit;
+        n /= 10;
+    }
+
+    return sum == product;
+}
