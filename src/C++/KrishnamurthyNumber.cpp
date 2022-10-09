52f2e675cc4e55911f2e135f2f02922b29cabf36
Moksh Upadhyay <68551326+MokshUpadhyay57@users.noreply.github.com>
2022-10-09 16:22:22 +0530

    Added Code for Krishna Murthy Number (#19)
    * Added Implementation of Goldbach Number in C++
    
    * Added Comment
    
    * feat : Added Implementation of Krishnamurthy Number in C++
    
    * Removed main function

---
+++ src/C++/KrishnamurthyNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+bool isKrishanMurthyNumber(long n);
+int fact(long n);
+
+int fact(long n) {
+    int fact = 1;
+    while (n > 0) {
+        fact *= n--;
+    }
+    return fact;
+}
+
+bool isKrishanMurthyNumber(long n) {
+    long temp = n;
+    long sum = 0;
+    while (temp > 0) {
+        sum += fact(temp % 10);
+        temp /= 10;
+    }
+    return sum == n;
+}
