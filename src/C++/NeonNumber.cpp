f38a98559ea87121392c5bcfd7773fe8ad264933
Moksh Upadhyay <mokshatwork@gmail.com>
2022-10-09 17:38:57 +0530

    feat : Added Implementation of Neon Number in C++

---
+++ src/C++/NeonNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+bool isNeonNumber(long n);
+
+bool isNeonNumber(long n) {
+    long sqr = n * n;
+    long sum = 0;
+    while (sqr > 0) {
+        sum = sum + sqr % 10;
+        sqr = sqr / 10;
+    }
+
+    return sum == n;
+}
