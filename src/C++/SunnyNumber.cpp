df228a6d7f7c3a46799d83fcd40892cd9d83e617
Moksh Upadhyay <68551326+MokshUpadhyay57@users.noreply.github.com>
2022-10-14 23:04:37 +0530

    feat : Added Implementation of Sunny Number in C++ Language (#28)

---
+++ src/C++/SunnyNumber.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+
+#include <iostream>
+
+using namespace std;
+bool isSunnyNumber(long n);
+
+bool isSunnyNumber(long n) {
+    double sqrD = sqrt(n + 1);
+    long sqr = (long)sqrD;
+    return sqr == sqrD;
+}
