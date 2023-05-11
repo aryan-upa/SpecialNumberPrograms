d0496fc90b636c6b114b62629283a0c9aef50191
Akshay <akshay.suryanarayan@gmail.com>
2023-05-11 23:20:27 +0530

    better implementation of checking for a  prime number in c++ (#36)
    * better implementation of checking for a  prime number in c++
    
    * better implementation of checking for a  prime number in c++

--- src/C++/PrimeNumber.cpp
+++ src/C++/PrimeNumber.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 bool isPrime(int n)
 {
-	if (n <= 1)
-		return false;
 
-	for (int i = 2; i < n; i++)
-		if (n % i == 0)
+	for (int divisor = 2; divisor * divisor <= n; divisor++)
+	{
+		if (n % divisor == 0)
+		{
 			return false;
+		}
+	}
 
-	return true;
+	return (n >= 2);
 }
